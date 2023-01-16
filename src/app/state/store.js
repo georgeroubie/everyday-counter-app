9eb8f8eca76b04c8a95cf43362b0c2ab94f7d01a
George Roubie <george.roubie@gmail.com>
2023-01-16 20:13:19 +0200

    Update store.js

--- src/app/state/store.js
+++ src/app/state/store.js
@@ -6,6 +6,12 @@ import { appReducer } from './reducer';
 function useAppState() {
   const [state, dispatch] = useReducer(appReducer, {
     theme: getCurrentTheme(),
+    list: [
+      {
+        id: 0,
+        name: 'Cigarets',
+      },
+    ],
   });
 
   function setState(type, value) {
@@ -17,9 +23,14 @@ function useAppState() {
     setState(actionTypes.UPDATE_THEME_SELECTION, value);
   }
 
+  function addNewCounter(value) {
+    setState(actionTypes.ADD_NEW_COUNTER, value);
+  }
+
   return {
     state,
     setTheme,
+    addNewCounter,
   };
 }
 
