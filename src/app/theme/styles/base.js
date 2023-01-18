5bfa67cc95478df85314e021459d14702a14cba1
George Roubie <george.roubie@gmail.com>
2023-01-18 20:58:56 +0200

    Update base.js

--- src/app/theme/styles/base.js
+++ src/app/theme/styles/base.js
@@ -14,6 +14,31 @@ const base = css`
 
   button {
     appearance: none;
+    min-width: 0;
+    border: 0;
+    user-select: none;
+
+    &:hover {
+      text-decoration: none;
+    }
+
+    &:focus,
+    &:active {
+      outline: 0;
+      box-shadow: ${({ theme: { spacing, colors } }) => `0 0 0 ${spacing.xsmall} ${colors.outline}`};
+      text-decoration: none;
+    }
+
+    &.disabled,
+    &:disabled {
+      opacity: 0.65;
+      box-shadow: none;
+      text-decoration: none;
+    }
+
+    &:not(:disabled) {
+      cursor: pointer;
+    }
   }
 `;
 
