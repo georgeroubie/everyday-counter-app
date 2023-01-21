1ae7aef30d213e15924f2d20e0097441efbb827f
George Roubie <george.roubie@gmail.com>
2023-01-21 13:56:49 +0200

    Update index.jsx

--- src/app/components/counter-list/index.jsx
+++ src/app/components/counter-list/index.jsx
@@ -1,16 +1,43 @@
 import { useContext } from 'react';
+import { useNavigate } from 'react-router-dom';
+import styled from 'styled-components';
 import { AppContext } from '../../state/Context';
 import Description from '../typography/Description';
-import Title from '../typography/Title';
+import _Title from '../typography/Title';
+import _Button from '../ui/Button';
 import ListItem from './ListItem';
 
+const TitleWrapper = styled.div`
+  display: flex;
+  align-items: center;
+  justify-content: space-between;
+  margin-bottom: ${({ theme: { spacing } }) => spacing.large};
+  gap: ${({ theme: { spacing } }) => spacing.large};
+`;
+
+const Title = styled(_Title)`
+  margin-bottom: 0;
+`;
+
+const Button = styled(_Button)`
+  width: auto;
+  flex-shrink: 0;
+`;
+
 const CounterList = () => {
+  const navigate = useNavigate();
   const { state } = useContext(AppContext);
   const { list } = state;
 
   return (
     <>
-      <Title>Counter List</Title>
+      <TitleWrapper>
+        <Title>My counters</Title>
+        <Button size="large" onClick={() => navigate('/add-new-counter')}>
+          Add new
+        </Button>
+      </TitleWrapper>
+
       {!list?.length ? (
         <Description>There are no counters, add a new one.</Description>
       ) : (
