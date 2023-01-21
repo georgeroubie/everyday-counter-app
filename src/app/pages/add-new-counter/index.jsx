1dbf6cd714e8a1768d488ecf6962cded149dfc1a
George Roubie <george.roubie@gmail.com>
2023-01-21 13:56:55 +0200

    Create index.jsx

---
+++ src/app/pages/add-new-counter/index.jsx
@@ -0,0 +1,12 @@
+import PageWrapper from '../../components/layout/PageWrapper';
+import Title from '../../components/typography/Title';
+
+const AddNewCounter = () => {
+  return (
+    <PageWrapper>
+      <Title>Add new counter</Title>
+    </PageWrapper>
+  );
+};
+
+export default AddNewCounter;
