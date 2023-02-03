0ffd0852748e2c35560be79c57948b0f3eb2558e
George Roubie <george.roubie@gmail.com>
2023-02-03 21:26:40 +0200

    Create input component

--- src/app/theme/styles/base.js
+++ src/app/theme/styles/base.js
@@ -40,6 +40,45 @@ const base = css`
       cursor: pointer;
     }
   }
+
+  input {
+    &:focus {
+      outline: 0;
+    }
+
+    /* Remove autocomplete background-color */
+
+    &:-webkit-autofill,
+    &:-webkit-autofill:hover,
+    &:-webkit-autofill:focus,
+    &:-webkit-autofill:active {
+      transition: background-color 5000s ease-in-out 0s;
+      transition-delay: 9999s;
+    }
+
+    /* clears the 'X' from Internet Explorer */
+
+    &::-ms-clear {
+      display: none;
+      width: 0;
+      height: 0;
+    }
+
+    &::-ms-reveal {
+      display: none;
+      width: 0;
+      height: 0;
+    }
+
+    /* clears the 'X' from Chrome */
+
+    &::-webkit-search-decoration,
+    &::-webkit-search-cancel-button,
+    &::-webkit-search-results-button,
+    &::-webkit-search-results-decoration {
+      display: none;
+    }
+  }
 `;
 
 export { base };
