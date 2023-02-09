533b3db1a84ca663ec364b1762e9907d4ab643b1
George Roubie <george.roubie@gmail.com>
2023-02-09 16:04:04 +0200

    Update index.jsx

--- src/app/components/counter-list/index.jsx
+++ src/app/components/counter-list/index.jsx
@@ -43,8 +43,7 @@ const CounterList = () => {
         <>
           <Description>Right now there are no counters, click the "Add new" button to create one.</Description>
           <Description>
-            Everyday counter web application, helps you count things for you, e.g. how many cups of water you are
-            drinking.
+            Everyday counter web application, helps you count things, e.g. how many cups of water you are drinking.
           </Description>
           <Description>Also helps you stop your bad habits, e.g. track how much you smoke every day.</Description>
           <Description>You can use it for whatever you need to track.</Description>
