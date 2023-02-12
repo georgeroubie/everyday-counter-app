b7a83f7dd72d67c3da107fad86c5b4351bee5b08
George Roubie <george.roubie@gmail.com>
2023-02-12 17:56:32 +0200

    Update NoCounters.jsx

--- src/app/pages/home/NoCounters.jsx
+++ src/app/pages/home/NoCounters.jsx
@@ -1,6 +1,23 @@
+import { useNavigate } from 'react-router-dom';
+import styled from 'styled-components';
+import Icons from '../../components/icons/Icons';
 import Description from '../../components/typography/Description';
 
+const Wrapper = styled.div`
+  display: flex;
+  align-items: center;
+  justify-content: center;
+  margin: ${({ theme: { spacing } }) => spacing.xlarge} auto;
+  border: ${({ theme: { colors, shapes } }) => `${shapes.divider} solid ${colors.borderPrimary}`};
+  width: 250px;
+  height: 250px;
+  border-radius: ${({ theme: { shapes } }) => shapes.circle};
+  cursor: pointer;
+`;
+
 const NoCounters = () => {
+  const navigate = useNavigate();
+
   return (
     <>
       <Description>Right now there are no counters, click the "Add new" button to create one.</Description>
@@ -9,6 +26,9 @@ const NoCounters = () => {
       </Description>
       <Description>Also helps you stop your bad habits, e.g. track how much you smoke every day.</Description>
       <Description>You can use it for whatever you need to track.</Description>
+      <Wrapper onClick={() => navigate('/add-new-counter')}>
+        <Icons type="Plus" width="70px" />
+      </Wrapper>
     </>
   );
 };
