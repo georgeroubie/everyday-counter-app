75008d98777da55d4c41a451dd730da3ff5f3444
George Roubie <george.roubie@gmail.com>
2023-02-12 17:08:52 +0200

    Create NoCounters.jsx

---
+++ src/app/pages/home/NoCounters.jsx
@@ -0,0 +1,16 @@
+import Description from '../../components/typography/Description';
+
+const NoCounters = () => {
+  return (
+    <>
+      <Description>Right now there are no counters, click the "Add new" button to create one.</Description>
+      <Description>
+        Everyday counter web application, helps you count things, e.g. how many cups of water you are drinking.
+      </Description>
+      <Description>Also helps you stop your bad habits, e.g. track how much you smoke every day.</Description>
+      <Description>You can use it for whatever you need to track.</Description>
+    </>
+  );
+};
+
+export default NoCounters;
