6f762770fd79002027df3e2a383a404adc0c0fab
George Roubie <george.roubie@gmail.com>
2023-02-12 17:08:50 +0200

    Update index.jsx

--- src/app/pages/home/index.jsx
+++ src/app/pages/home/index.jsx
@@ -2,11 +2,11 @@ import { useContext } from 'react';
 import { useNavigate } from 'react-router-dom';
 import styled from 'styled-components';
 import PageWrapper from '../../components/layout/PageWrapper';
-import Description from '../../components/typography/Description';
 import _Title from '../../components/typography/Title';
 import _Button from '../../components/ui/Button';
 import { AppContext } from '../../state/Context';
 import ListItem from './ListItem';
+import NoCounters from './NoCounters';
 
 const TitleWrapper = styled.div`
   display: flex;
@@ -38,18 +38,7 @@ const Home = () => {
           Add new
         </Button>
       </TitleWrapper>
-      {Boolean(list.length) ? (
-        list.map((listItem) => <ListItem key={listItem.id} data={listItem} />)
-      ) : (
-        <>
-          <Description>Right now there are no counters, click the "Add new" button to create one.</Description>
-          <Description>
-            Everyday counter web application, helps you count things, e.g. how many cups of water you are drinking.
-          </Description>
-          <Description>Also helps you stop your bad habits, e.g. track how much you smoke every day.</Description>
-          <Description>You can use it for whatever you need to track.</Description>
-        </>
-      )}
+      {Boolean(list.length) ? list.map((listItem) => <ListItem key={listItem.id} data={listItem} />) : <NoCounters />}
     </PageWrapper>
   );
 };
