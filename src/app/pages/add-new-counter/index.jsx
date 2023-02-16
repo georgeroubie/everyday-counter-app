f37ff99dd7f04e9809dfebb2c80cf1c2e75d457b
George Roubie <george.roubie@gmail.com>
2023-02-16 12:11:26 +0200

    Update index.jsx

--- src/app/pages/add-new-counter/index.jsx
+++ src/app/pages/add-new-counter/index.jsx
@@ -21,11 +21,28 @@ const Button = styled(_Button)`
   width: 50%;
 `;
 
+const ErrorMessage = styled.div`
+  color: ${({ theme: { colors } }) => colors.errorPrimary};
+  font-weight: ${({ theme: { fontWeight } }) => fontWeight.bold};
+  margin-top: -${({ theme: { spacing } }) => spacing.normal};
+  margin-bottom: ${({ theme: { spacing } }) => spacing.large};
+`;
+
 const AddNewCounter = () => {
   const navigate = useNavigate();
   const { addNewCounter } = useContext(AppContext);
   const [name, setName] = useState('');
   const [goal, setGoal] = useState();
+  const [limit, setLimit] = useState();
+  const [showLimitErrorMessage, setShowLimitErrorMessage] = useState(false);
+
+  function isValidValue(value) {
+    return value.match(/^-?\d+$/) && value < 100000000;
+  }
+
+  function saveValue(value, setValue) {
+    setValue(Math.abs(value));
+  }
 
   function goToHome() {
     navigate('/');
@@ -33,6 +50,25 @@ const AddNewCounter = () => {
 
   function onNameChange({ target }) {
     setName(target.value);
+  }
+
+  function onLimitChange({ target }) {
+    const { value } = target;
+
+    if (!value) {
+      setLimit();
+      return;
+    }
+
+    if (isValidValue(value)) {
+      if (goal && value < goal) {
+        setShowLimitErrorMessage(true);
+      } else {
+        setShowLimitErrorMessage(false);
+      }
+
+      saveValue(value, setLimit);
+    }
   }
 
   function onGoalChange({ target }) {
@@ -43,8 +79,13 @@ const AddNewCounter = () => {
       return;
     }
 
-    if (value.match(/^-?\d+$/) && value < 100000000) {
-      setGoal(Math.abs(target.value));
+    if (isValidValue(value)) {
+      if (limit && value > limit) {
+        setShowLimitErrorMessage(true);
+      } else {
+        setShowLimitErrorMessage(false);
+      }
+      saveValue(value, setGoal);
     }
   }
 
@@ -53,6 +94,7 @@ const AddNewCounter = () => {
       id: `${new Date().valueOf()}`,
       name,
       goal,
+      limit,
       value: 0,
     });
     goToHome();
@@ -63,13 +105,16 @@ const AddNewCounter = () => {
       <Title>Add a new counter</Title>
       <Description>Use a friendly name for the counter, e.g. "Cups of water".</Description>
       <Input label="Counter name" value={name} onChange={onNameChange} />
-      <Description>Optionally add a goal to keep you motivated!</Description>
+      <Description>Add a goal to keep you motivated</Description>
       <Input label="Counter goal" inputMode="numeric" value={goal} onChange={onGoalChange} />
+      <Description>Add a limit to help you control yourself</Description>
+      <Input label="Counter limit" inputMode="numeric" value={limit} onChange={onLimitChange} />
+      {showLimitErrorMessage && <ErrorMessage>Limit must be bigger than goal</ErrorMessage>}
       <ButtonWrapper>
         <Button variation="secondary" size="large" onClick={goToHome}>
           CANCEL
         </Button>
-        <Button size="large" disabled={!name} onClick={save}>
+        <Button size="large" disabled={!name || showLimitErrorMessage} onClick={save}>
           SAVE
         </Button>
       </ButtonWrapper>
