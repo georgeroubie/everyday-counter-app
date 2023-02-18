ebc324cfdf92097d6dfca3dd43b0790be3338bbf
George Roubie <george.roubie@gmail.com>
2023-02-18 19:25:53 +0200

    Update CounterList.jsx

--- src/app/pages/home/CounterList.jsx
+++ src/app/pages/home/CounterList.jsx
@@ -1,13 +1,20 @@
 import { useContext } from 'react';
+import styled from 'styled-components';
 import { AppContext } from '../../state/Context';
 import Counter from './Counter';
 import NoCounters from './NoCounters';
+
+const Wrapper = styled.div``;
 
 const CounterList = () => {
   const { state } = useContext(AppContext);
   const { list } = state;
 
-  return Boolean(list.length) ? list.map((counter) => <Counter key={counter.id} data={counter} />) : <NoCounters />;
+  return (
+    <Wrapper>
+      {Boolean(list.length) ? list.map((counter) => <Counter key={counter.id} data={counter} />) : <NoCounters />}
+    </Wrapper>
+  );
 };
 
 export default CounterList;
