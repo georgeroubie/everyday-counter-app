69e68c821962989fa4fa18a240ded6c168e1b867
George Roubie <george.roubie@gmail.com>
2023-02-18 19:25:57 +0200

    Update NoCounters.jsx

--- src/app/pages/home/NoCounters.jsx
+++ src/app/pages/home/NoCounters.jsx
@@ -1,28 +1,15 @@
 import { useNavigate } from 'react-router-dom';
 import styled from 'styled-components';
-import Icons from '../../components/icons/Icons';
+import Description from '../../components/typography/Description';
 
-const Wrapper = styled.div`
-  display: flex;
-  align-items: center;
-  justify-content: center;
-  position: fixed;
-  top: 50%;
-  left: 50%;
-  transform: translate(-50%, -50%);
-  border: ${({ theme: { colors, shapes } }) => `${shapes.divider} solid ${colors.borderPrimary}`};
-  width: 160px;
-  height: 160px;
-  border-radius: ${({ theme: { shapes } }) => shapes.circle};
-  cursor: pointer;
-`;
+const Wrapper = styled.div``;
 
 const NoCounters = () => {
   const navigate = useNavigate();
 
   return (
     <Wrapper onClick={() => navigate('/add-new-counter')}>
-      <Icons type="Plus" width="40px" />
+      <Description>There are no counters right now, add new.</Description>
     </Wrapper>
   );
 };
