4cf124f433308c3d2c08bcfc561fd2e5363d011f
George Roubie <george.roubie@gmail.com>
2023-02-18 19:51:46 +0200

    Update AddNewCounterButton.jsx

--- src/app/pages/home/AddNewCounterButton.jsx
+++ src/app/pages/home/AddNewCounterButton.jsx
@@ -1,30 +1,19 @@
 import { useNavigate } from 'react-router-dom';
 import styled from 'styled-components';
+import BottomArea from '../../components/layout/BottomArea';
 import _Button from '../../components/ui/Button';
 
-const Wrapper = styled.div`
-  position: fixed;
-  left: 0;
-  bottom: 0;
-  padding: ${({ theme: { spacing } }) => spacing.large};
-  width: 100%;
-  background-color: ${({ theme: { colors } }) => colors.background};
-`;
-
-const Button = styled(_Button)`
-  max-width: 960px;
-  margin: 0 auto;
-`;
+const Button = styled(_Button)``;
 
 const AddNewCounterButton = () => {
   const navigate = useNavigate();
 
   return (
-    <Wrapper>
+    <BottomArea>
       <Button size="large" onClick={() => navigate('/add-new-counter')}>
         Add new
       </Button>
-    </Wrapper>
+    </BottomArea>
   );
 };
 
