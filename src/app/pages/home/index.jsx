26928379a3ba20ef2491ad1f36e87d45a4209072
George Roubie <george.roubie@gmail.com>
2023-02-18 19:25:56 +0200

    Update index.jsx

--- src/app/pages/home/index.jsx
+++ src/app/pages/home/index.jsx
@@ -1,10 +1,9 @@
-import { useContext } from 'react';
-import { useNavigate } from 'react-router-dom';
+import { NavLink as _NavLink } from 'react-router-dom';
 import styled from 'styled-components';
+import _Icons from '../../components/icons/Icons';
 import PageWrapper from '../../components/layout/PageWrapper';
 import _Title from '../../components/typography/Title';
-import _Button from '../../components/ui/Button';
-import { AppContext } from '../../state/Context';
+import AddNewCounterButton from './AddNewCounterButton';
 import CounterList from './CounterList';
 
 const TitleWrapper = styled.div`
@@ -19,27 +18,31 @@ const Title = styled(_Title)`
   margin-bottom: 0;
 `;
 
-const Button = styled(_Button)`
-  width: auto;
-  flex-shrink: 0;
+const NavLink = styled(_NavLink)`
+  text-decoration: none;
+  padding: ${({ theme: { spacing } }) => spacing.normal};
+  background-color: ${({ theme: { colors } }) => colors.background};
+  display: inline-block;
+  border-radius: ${({ theme: { shapes } }) => shapes.circle};
+  width: 3rem;
+  height: 3rem;
+`;
+
+const Icons = styled(_Icons)`
+  color: ${({ theme: { colors } }) => colors.textPrimary};
 `;
 
 const Home = () => {
-  const { state } = useContext(AppContext);
-  const { list } = state;
-  const navigate = useNavigate();
-
   return (
     <PageWrapper>
-      {Boolean(list.length) && (
-        <TitleWrapper>
-          <Title>Counters</Title>
-          <Button size="large" onClick={() => navigate('/add-new-counter')}>
-            Add new
-          </Button>
-        </TitleWrapper>
-      )}
+      <TitleWrapper>
+        <Title>My counters</Title>
+        <NavLink to="/info" end>
+          <Icons type="Gear" />
+        </NavLink>
+      </TitleWrapper>
       <CounterList />
+      <AddNewCounterButton />
     </PageWrapper>
   );
 };
