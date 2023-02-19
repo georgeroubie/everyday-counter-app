27277e14c5fb8530a556075db124d4959d20a93a
George Roubie <george.roubie@gmail.com>
2023-02-19 02:23:09 +0200

    Update CounterList.jsx

--- src/app/pages/home/CounterList.jsx
+++ src/app/pages/home/CounterList.jsx
@@ -1,19 +1,35 @@
 import { useContext } from 'react';
 import styled from 'styled-components';
 import { AppContext } from '../../state/Context';
-import Counter from './Counter';
+import _Counter from './Counter';
 import NoCounters from './NoCounters';
 
-const Wrapper = styled.div``;
+const Wrapper = styled.div`
+  @media (${({ theme: { breakpoints } }) => breakpoints.lg}) {
+    display: grid;
+    grid-template-columns: repeat(2, 1fr);
+    gap: ${({ theme: { spacing } }) => spacing.large};
+  }
+`;
+
+const Counter = styled(_Counter)`
+  @media (${({ theme: { breakpoints } }) => breakpoints.lg}) {
+    margin-bottom: 0;
+  }
+`;
 
 const CounterList = () => {
   const { state } = useContext(AppContext);
   const { list } = state;
 
-  return (
+  return Boolean(list.length) ? (
     <Wrapper>
-      {Boolean(list.length) ? list.map((counter) => <Counter key={counter.id} data={counter} />) : <NoCounters />}
+      {list.map((counter) => (
+        <Counter key={counter.id} data={counter} />
+      ))}
     </Wrapper>
+  ) : (
+    <NoCounters />
   );
 };
 
