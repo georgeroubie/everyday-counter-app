f8acdb33d4b9d1c8f6c3cff1e9872385d7b6ee87
George Roubie <george.roubie@gmail.com>
2023-02-23 14:59:44 +0200

    Update PageWrapper.jsx

--- src/app/components/layout/PageWrapper.jsx
+++ src/app/components/layout/PageWrapper.jsx
@@ -2,7 +2,12 @@ import PropTypes from 'prop-types';
 import styled from 'styled-components';
 
 const Wrapper = styled.main`
+  position: relative;
   margin: ${({ theme: { spacing } }) => `${spacing.large} auto ${spacing.xxlarge}`};
+
+  @media (${({ theme: { breakpoints } }) => breakpoints.lg}) {
+    max-width: 390px;
+  }
 `;
 
 const PageWrapper = ({ children }) => {
