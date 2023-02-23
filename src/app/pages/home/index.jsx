eaa031fa1f2d499f3b997156636ee2528a6e6f67
George Roubie <george.roubie@gmail.com>
2023-02-23 14:59:54 +0200

    Update index.jsx

--- src/app/pages/home/index.jsx
+++ src/app/pages/home/index.jsx
@@ -2,21 +2,20 @@ import { NavLink as _NavLink } from 'react-router-dom';
 import styled from 'styled-components';
 import _Icons from '../../components/icons/Icons';
 import PageWrapper from '../../components/layout/PageWrapper';
-import _Title from '../../components/typography/Title';
+import logo from './../../../assets/img/logo.svg';
 import AddNewCounterButton from './AddNewCounterButton';
 import CounterList from './CounterList';
 
 const TitleWrapper = styled.div`
   display: flex;
   align-items: center;
-  justify-content: space-between;
+  justify-content: center;
   margin-bottom: ${({ theme: { spacing } }) => spacing.large};
   gap: ${({ theme: { spacing } }) => spacing.large};
+  position: relative;
 `;
 
-const Title = styled(_Title)`
-  margin-bottom: 0;
-`;
+const Image = styled.img``;
 
 const NavLink = styled(_NavLink)`
   text-decoration: none;
@@ -24,8 +23,16 @@ const NavLink = styled(_NavLink)`
   background-color: ${({ theme: { colors } }) => colors.background};
   display: inline-block;
   border-radius: ${({ theme: { shapes } }) => shapes.circle};
-  width: 3rem;
-  height: 3rem;
+  width: 2.3rem;
+  height: 2.3rem;
+  position: absolute;
+  right: 0;
+  top: 50%;
+  transform: translateY(-50%);
+
+  @media (${({ theme: { breakpoints } }) => breakpoints.lg}) {
+    right: -100px;
+  }
 `;
 
 const Icons = styled(_Icons)`
@@ -36,7 +43,7 @@ const Home = () => {
   return (
     <PageWrapper>
       <TitleWrapper>
-        <Title>My counters</Title>
+        <Image src={logo} alt="Everyday counter app" />
         <NavLink to="/info" end>
           <Icons type="Gear" />
         </NavLink>
