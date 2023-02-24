d90911a505bc04bf4ea3ba194fa305d11199333d
George Roubie <george.roubie@gmail.com>
2023-02-24 22:09:12 +0200

    Update CounterList.jsx

--- src/app/pages/home/CounterList.jsx
+++ src/app/pages/home/CounterList.jsx
@@ -1,22 +1,10 @@
 import { useContext } from 'react';
 import styled from 'styled-components';
 import { AppContext } from '../../state/Context';
-import _Counter from './Counter';
+import Counter from './Counter';
 import NoCounters from './NoCounters';
 
-const Wrapper = styled.div`
-  @media (${({ theme: { breakpoints } }) => breakpoints.lg}) {
-    display: grid;
-    grid-template-columns: repeat(2, 1fr);
-    gap: ${({ theme: { spacing } }) => spacing.large};
-  }
-`;
-
-const Counter = styled(_Counter)`
-  @media (${({ theme: { breakpoints } }) => breakpoints.lg}) {
-    margin-bottom: 0;
-  }
-`;
+const Wrapper = styled.div``;
 
 const CounterList = () => {
   const { state } = useContext(AppContext);
