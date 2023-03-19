201b7e1d694eda66219023741e209e87d03b9f52
George Roubie <george.roubie@gmail.com>
2023-03-19 19:11:57 +0200

    Add env(safe-area-inset-bottom)

--- src/app/components/layout/BottomArea.jsx
+++ src/app/components/layout/BottomArea.jsx
@@ -5,7 +5,7 @@ const Wrapper = styled.div`
   position: fixed;
   left: 0;
   bottom: 0;
-  padding: ${({ theme: { spacing } }) => `${spacing.normal} ${spacing.large}`};
+  padding-bottom: env(safe-area-inset-bottom);
   width: 100%;
   background-color: ${({ theme: { colors } }) => colors.background};
   z-index: ${({ theme: { zIndex } }) => zIndex.fixed};
@@ -20,9 +20,11 @@ const Wrapper = styled.div`
 const Items = styled.div`
   max-width: 630px;
   margin: 0 auto;
+  padding: ${({ theme: { spacing } }) => `${spacing.normal} ${spacing.large}`};
 
   @media (${({ theme: { breakpoints } }) => breakpoints.lg}) {
     margin-top: ${({ theme: { spacing } }) => spacing.xlarge};
+    padding: 0;
   }
 `;
 
