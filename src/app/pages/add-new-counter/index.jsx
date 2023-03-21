c67ae655685487931f4ec45ff4d7e28d98350984
George Roubie <george.roubie@gmail.com>
2023-03-21 10:30:37 +0200

    Use the counter form component in the add new counter page

--- src/app/pages/add-new-counter/index.jsx
+++ src/app/pages/add-new-counter/index.jsx
@@ -1,122 +1,27 @@
-import { useContext, useEffect, useState } from 'react';
+import { useContext } from 'react';
 import { useNavigate } from 'react-router-dom';
-import styled from 'styled-components';
-import BottomArea from '../../components/layout/BottomArea';
 import PageWrapper from '../../components/layout/PageWrapper';
 import Title from '../../components/typography/Title';
-import _Button from '../../components/ui/Button';
-import Checkbox from '../../components/ui/Checkbox';
-import _Input from '../../components/ui/Input';
+import CounterForm from '../../components/ui/CounterForm';
 import { AppContext } from '../../state/Context';
-
-const Input = styled(_Input)`
-  margin-bottom: ${({ theme: { spacing } }) => spacing.large};
-`;
-
-const ButtonWrapper = styled.div`
-  display: flex;
-  gap: ${({ theme: { spacing } }) => spacing.large};
-`;
-
-const Button = styled(_Button)`
-  width: 50%;
-`;
-
-const ErrorMessage = styled.div`
-  color: ${({ theme: { colors } }) => colors.errorPrimary};
-  font-weight: ${({ theme: { fontWeight } }) => fontWeight.bold};
-  margin-top: -${({ theme: { spacing } }) => spacing.normal};
-  margin-bottom: ${({ theme: { spacing } }) => spacing.large};
-`;
 
 const AddNewCounter = () => {
   const navigate = useNavigate();
   const { addNewCounter } = useContext(AppContext);
-  const [name, setName] = useState('');
-  const [goal, setGoal] = useState();
-  const [limit, setLimit] = useState();
-  const [showLimitErrorMessage, setShowLimitErrorMessage] = useState(false);
-  const [reset, setReset] = useState(false);
-
-  useEffect(() => {
-    if (!goal || !limit) {
-      setShowLimitErrorMessage(false);
-    }
-  }, [goal, limit]);
 
   function goToHome() {
     navigate('/');
   }
 
-  function isValidValue(value) {
-    return value.match(/^-?\d+$/) && value < 100000000;
-  }
-
-  function saveValue(value, setValue) {
-    setValue(Math.abs(value));
-  }
-
-  function onNameChange({ target }) {
-    setName(target.value);
-  }
-
-  function onLimitChange({ target }) {
-    const { value } = target;
-
-    if (!value) {
-      setLimit();
-      return;
-    }
-
-    if (isValidValue(value)) {
-      const hasError = Boolean(goal && value < goal);
-      setShowLimitErrorMessage(hasError);
-      saveValue(value, setLimit);
-    }
-  }
-
-  function onGoalChange({ target }) {
-    const { value } = target;
-
-    if (!value) {
-      setGoal();
-      return;
-    }
-
-    if (isValidValue(value)) {
-      const hasError = Boolean(limit && value > limit);
-      setShowLimitErrorMessage(hasError);
-      saveValue(value, setGoal);
-    }
-  }
-
-  function save() {
-    const now = new Date();
-    const id = `${now.valueOf()}`;
-    const day = now.getDate();
-    const value = 0;
-    addNewCounter({ id, name, goal, limit, value, reset, day });
+  function saveCounter(counterData) {
+    addNewCounter(counterData);
     goToHome();
   }
 
   return (
     <PageWrapper>
       <Title onBack={goToHome}>Add a new counter</Title>
-      <Input label="*Name" value={name} onChange={onNameChange} />
-      <Input label="Goal" inputMode="numeric" value={goal} onChange={onGoalChange} />
-      <Input label="Limit" inputMode="numeric" value={limit} onChange={onLimitChange} />
-      {showLimitErrorMessage && <ErrorMessage>Limit must be bigger than goal</ErrorMessage>}
-      <Checkbox label="Reset at midnight" checked={reset} onChange={setReset} />
-      <BottomArea>
-        <ButtonWrapper>
-          <Button variation="secondary" size="large" onClick={goToHome}>
-            CANCEL
-          </Button>
-          <Button size="large" disabled={!name || showLimitErrorMessage} onClick={save}>
-            SAVE
-          </Button>
-        </ButtonWrapper>
-      </BottomArea>
+      <CounterForm onSave={saveCounter} />
     </PageWrapper>
   );
 };
